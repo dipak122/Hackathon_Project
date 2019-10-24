444423347cafd0d23da9a1cfbf13855d21406843
Dipak <49452278+dipak122@users.noreply.github.com>
2019-10-24 22:14:48 +0530

    updated

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -159,5 +159,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dipak515gautam@gmail.com'
-EMAIL_HOST_PASSWORD = 'Saurabh@!2019'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_PORT = 587
